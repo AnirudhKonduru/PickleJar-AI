167f79fb213366a59b81487027058b8c7d839dce
Aravind B.S <bs.aravind.97@gmail.com>
2017-10-27 02:46:45 +0530

    twitter replies

--- PickleJar/pickles/tweet.py
+++ PickleJar/pickles/tweet.py
@@ -11,9 +11,12 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
+
+
 triggers = {
 
-'tweet':[['send','tweet']]
+'tweet':[['send','tweet']],
+'read_tweet':[['read', 'tweet']]
 
 }
 
@@ -22,6 +25,38 @@ def tweet(s):
     tweet = listen()
     api.update_status(status= tweet)
     return "Tweet Sent"
+
+
+def read_tweet(s):
+    mentions = api.mentions_timeline(count=1)
+
+    for mention in mentions:
+        a = mention.text
+        b = mention.user.screen_name
+        c = mention.id
+
+    print(c)
+
+    say("your last tweet is "+a +" by "+b)
+
+    say("Do you want me to reply?")
+    choice = listen()
+
+    if choice == "yes":
+
+        say("What should i tweet?")
+        text = listen()
+        tweet = "@" + b + " " + text
+
+        api.update_status(tweet, c)
+        return "Tweet Sent"
+
+
+
+
+    return "tweet read."
+
+
 
 
 
