faabd1b4b81ce176ae1c9d0990d83e43cbbff7df
Aravind B.S <bs.aravind.97@gmail.com>
2017-10-28 10:43:32 +0530

    test

--- PickleJar/pickle.py
+++ PickleJar/pickle.py
@@ -12,11 +12,13 @@ def init():
     config["input"] = "text"
     config["output"] = "text"
 
+'''
     for i in range(0, len(sys.argv)-1):
         if (sys.argv[i] == "-i" or "--input") and (sys.argv[i+1] == "text" or "speech"):
             config["input"] = sys.argv[i+1]
         if (sys.argv[i] == "-o" or "--output") and (sys.argv[i+1] == "text" or "speech"):
             config["output"] = sys.argv[i+1]
-
+'''
+    print(sys.argv)
     with open("config.yaml", "w") as stream:
         yaml.dump(config, stream)
